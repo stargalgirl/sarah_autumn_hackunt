67c868222b3619f08b9e19481058d7988b794b1d
sarah <sarah@dynamic-10-125-215-69.dynamic.unt.edu>
2019-04-06 17:39:06 -0500

    added

--- src/App.js
+++ src/App.js
@@ -12,11 +12,9 @@ class App extends Component {
         <div>
           <p>SERVERE WEATHER CONDITION:</p>
           <h1>TORNADO</h1>
-          <div>
             <button className="button">
               <img className="SOSButton" src="../SOSButton.png" alt="SOS Button to call emergency services" onClick={this.myfunction} />
             </button>
-          </div>
         </div>
       </div>
     );
