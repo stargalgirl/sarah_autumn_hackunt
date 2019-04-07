acdd1c66fda4353dcd2098eb87aaac6c27fda4a3
sarah <sarah@dynamic-10-125-215-69.dynamic.unt.edu>
2019-04-06 19:17:40 -0500

    dialog

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './Menu'
-
 class App extends Component {
+
   render() {
+    console.log()
     return (
       <div className="App">
         <Menu></Menu>
@@ -15,6 +16,11 @@ class App extends Component {
             <button className="button">
               <img className="SOSButton" src="../SOSButton.png" alt="SOS Button to call emergency services" onClick={this.myfunction} />
             </button>
+        </div>
+        <br></br>
+        <div classname="containerForButtons">
+              <button ><img className="twoButtons"src="../ImSafe.png" alt="Safe Button to notify contacts youre safe" onClick={this.myfunction} /></button>
+              <button><img className="twoButtons" src="../InDangerButton.png" alt="In Danger Button to notify contacts youre safe" onClick={this.myfunction} /></button>
         </div>
       </div>
     );
